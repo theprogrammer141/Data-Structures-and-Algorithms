f8987ec68a1e29112d21c9c18246de2d21024afe
theprogrammer141 <abdullah.k9913@gmail.com>
2024-10-17 15:29:21 +0500

    Added

---
+++ STACKS/src/NextGreaterElement/Main.java
@@ -0,0 +1,30 @@
+package NextGreaterElement;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {6, 8, 0, 1, 3};
+        Stack<Integer> s = new Stack<>();
+        int[] nextGreater = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            if(s.isEmpty()){
+                nextGreater[i] = -1;
+            }
+            else{
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+
+        for (int j : nextGreater) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
